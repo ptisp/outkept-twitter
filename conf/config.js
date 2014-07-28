14d88904176cb7eec63c2c2165789c502786df2e
Pedro Dias <petermdias@gmail.com>
2014-07-28 19:19:21 +0100

    initial commit

---
+++ conf/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'mongo_host': '127.0.0.1',
+  'mongo_port': '27017',
+  'mongo_database': 'outkept',
+
+  'twitter_consumer_key': process.env.TWITTER_CONSUMER_KEY,
+  'twitter_consumer_secret': process.env.TWITTER_CONSUMER_SECRET,
+  'twitter_access_token': process.env.TWITTER_ACCESS_TOKEN,
+  'twitter_token_secret': process.env.TWITTER_TOKEN_SECRET
+};
---
+++ main.js
@@ -0,0 +1,27 @@
+var vendors = require('./vendors'),
+  config = require('../conf/config'),
+  Ntwitter = require('ntwitter');
+
+var twitter = new Ntwitter({
+  consumer_key: config.twitter_consumer_key,
+  consumer_secret: config.twitter_consumer_secret,
+  access_token_key: config.twitter_access_token,
+  access_token_secret: config.twitter_token_secret
+});
+
+
+vendors.mongopubsub.subscribe('events', function (event) {
+  console.log(event);
+  var message = 'Server ' + event.hostname + ' ' + event.level + ' with ' + event.value + ' ' + event.sensor;
+  twitter.updateStatus(message, function (err, data) {
+    if(err) return console.log(err);
+      console.log(data);
+  });
+});
+
+
+/*
+vendors.mongopubsub.subscribe('messages', function (message) {
+  console.log(message);
+});
+*/
